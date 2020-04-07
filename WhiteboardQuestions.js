be9981ec4c2d5a0853d5fd4d4ce720936b068e39
jansenv <jansenv44@gmail.com>
2020-04-07 18:24:16 -0500

    did tech q 2

--- WhiteboardQuestions.js
+++ WhiteboardQuestions.js
@@ -5,11 +5,25 @@
     //     return self.indexOf(value) === index;
     // }
 
-    // arrayOfNums = [1, 2, 3, 1, 4, 5];
+    // let arrayOfNums = [1, 2, 3, 1, 4, 5];
 
     // const distinctNums = arrayOfNums.filter(distinct)
 
     // console.log(distinctNums)
 
 // 2
-// Given a list of numbers, extract the number that is the highest from the list. What if you wanted the highest number and the second highest number?
+// Given a list of numbers, extract the number that is the highest from the list. 
+
+    // let arrayOfNums = [1, 2, 4, 8, 16, 32, 64, 128]
+
+    // let maxNum = Math.max(...arrayOfNums);
+
+    // console.log(maxNum)
+
+//What if you wanted the highest number and the second highest number?
+
+    // arrayOfNums.splice(arrayOfNums.indexOf(maxNum), 1);
+
+    // let maxNum2 = Math.max(...arrayOfNums);
+
+    // console.log(maxNum2);
