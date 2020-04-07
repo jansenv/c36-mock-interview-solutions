89d1d3ff9e79d743a6d2949a125b942bb507994c
jansenv <jansenv44@gmail.com>
2020-04-07 18:02:56 -0500

    refactoring for organization

---
+++ WhiteboardQuestions.js
@@ -0,0 +1,15 @@
+// 1
+// Given an array [1, 2, 3, 1, 4, 5], write a function in JavaScript to create a new array with only the unique numbers.
+
+    // const distinct = (value, index, self) => {
+    //     return self.indexOf(value) === index;
+    // }
+
+    // arrayOfNums = [1, 2, 3, 1, 4, 5];
+
+    // const distinctNums = arrayOfNums.filter(distinct)
+
+    // console.log(distinctNums)
+
+// 2
+// Given a list of numbers, extract the number that is the highest from the list. What if you wanted the highest number and the second highest number?
